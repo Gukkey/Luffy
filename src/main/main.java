f2b343609a78de9f8b1d7c2b782ac1b7bf93f17e
Gukkey <poetguhi@gmail.com>
2022-06-13 10:14:38 +0530

    Base file, bot is not yet created.

---
+++ src/main/main.java
@@ -0,0 +1,30 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import reactor.core.publisher.Mono;
+
+public class MyBot {
+    public static void main(String[] args) {
+        DiscordClient client = DiscordClient.create("TOKEN");
+    }
+}
+
+public static void main(String[]args){
+
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+
+        login.block();
+
+        DiscordClient.create("TOKEN")
+        .withGateway(client ->
+        client.on(MessageCreateEvent.class, event -> {
+        Message message = event.getMessage();
+
+        if (message.getContent().equalsIgnoreCase("!ping")) {
+        return message.getChannel()
+        .flatMap(channel -> channel.createMessage("Pong!"));
+        }
+
+        return Mono.empty();
+        }))
+        .block();
+        }
